657ae78b384024e4745c62158e0071b3556279ee
Ajay Chenampara <ajay.chenampara@gmail.com>
2017-01-24 21:38:37 -0500

    clean

--- setup.py
+++ setup.py
@@ -9,13 +9,15 @@
 """
 
 import sys
+import versioneer
 from setuptools import setup
 
 
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
+    setup(version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
           use_pyscaffold=True)
 
 
--- virlsdk/__init__.py
+++ virlsdk/__init__.py
@@ -5,3 +5,7 @@ try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except:
     __version__ = 'unknown'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
